4a057072a02b5932448bf3ac7932a0a934f4cb96
Corentin Cadiou <cadiou@horizon.iap.fr>
2016-03-14 16:17:51 +0100

    working sort_galay
    Now opening correctly files

--- sort_galaxy.f90
+++ sort_galaxy.f90
@@ -1,12 +1,13 @@
 program sort_galaxy
   implicit none
   character(LEN=128) :: gal_list_filename, repository, outfile, fileinfo='none', halogalfile
-  character(LEN=128) :: dm_halo_list_filename
+  character(LEN=128) :: dm_halo_list_filename, dm_gal_assoc, associations_filename
   logical :: verbose
   integer :: ngal, gal_cols, i
   integer :: ndmhalo, dm_halo_cols
+  integer :: nassoc, assoc_cols
   integer :: ellipticals = 0, spirals = 0, others = 0
-  real(kind=4), dimension(:,:), allocatable :: data
+  real(kind=4), dimension(:,:), allocatable :: data_gal, data_halo
   real(kind=4), dimension(:, :), allocatable :: associations ! contains association flag / dark matter id / mass / gal id / mass
   real(kind=4) :: dmhalo_id, lvl, dmhalo_mass, gal_id, gal_mass
   real(kind=4), dimension(:), allocatable :: kind ! 0 for elliptical, 1 for spirals, 2 for other
@@ -26,35 +27,46 @@ program sort_galaxy
      stop
   end if
 
-  print*, 'Reading file', gal_list_filename
+  print*, 'Reading file "' // trim(gal_list_filename) // '"'
   open(unit=11, file=gal_list_filename, form='unformatted')
   read(11) ngal, gal_cols
   print*, 'Got', ngal, 'galaxies in da pocket'
 
-  print*, 'Reading file', dm_halo_list_filename
-  open(unit=12, file=dm_halo_list_filename, form='unformatted')
-  read(12) ndmhalo, dm_halo_cols
+  print*, ''
+  print*, 'Reading file "' // trim(associations_filename) // '"'
+  open(unit=12, file=associations_filename, form='unformatted')
+  read(12) nassoc, assoc_cols
+  print*, 'Got', nassoc, 'associations in da pocket'
+
+  print*, ''
+  print*, 'Reading file "' // trim(dm_halo_list_filename) // '"'
+  open(unit=13, file=dm_halo_list_filename, form='unformatted')
+  read(13) ndmhalo, dm_halo_cols
+  print*, 'Got', ndmhalo, 'halos in da pocket'
+
   !-------------------------------------
   ! Allocations & datareading
   !-------------------------------------
-  allocate(data(ngal, gal_cols+1))
-  allocate(associations(ndmhalo, dm_halo_cols+1))
+  allocate(data_gal(ngal, gal_cols+1))
+  allocate(data_halo(ndmhalo, dm_halo_cols+1))
+  allocate(associations(nassoc, assoc_cols+1))
 
   allocate(kind(ngal))
 
-  read(11) data(1:ngal, 1:gal_cols)
-  read(12) associations(1:ndmhalo, 1:dm_halo_cols)
+  read(11) data_gal(1:ngal, 1:gal_cols)
+  read(12) associations(1:nassoc, 1:assoc_cols)
+  read(13) data_halo(1:ndmhalo, 1:dm_halo_cols)
 
   !-------------------------------------
   ! Processing
   !-------------------------------------
   do i = 1, ngal
-     sigma = 1d0/3d0 * sqrt(data(i, 3)**2 + data(i, 4)**2 + data(i, 5)**2)
-     if (sigma / data(i, 2) > elliptical_threshold) then
-        data(i, gal_cols+1) = 0
+     sigma = 1d0/3d0 * sqrt(data_gal(i, 3)**2 + data_gal(i, 4)**2 + data_gal(i, 5)**2)
+     if (sigma / data_gal(i, 2) > elliptical_threshold) then
+        data_gal(i, gal_cols+1) = 0
         ellipticals = ellipticals + 1
-     else if (sigma / data(i, 2) < spiral_threshold) then
-        data(i, gal_cols+1) = 1
+     else if (sigma / data_gal(i, 2) < spiral_threshold) then
+        data_gal(i, gal_cols+1) = 0
         spirals = spirals + 1
      else
         kind(i) = 2
@@ -67,23 +79,16 @@ program sort_galaxy
   print*, spirals*100./ngal, "% spirals"
   print*, 100 - (ellipticals + spirals)*100./ngal, "% others"
 
-  !-------------------------------------
-  ! Load halo-galaxy association file halo
-  !-------------------------------------
-  open(unit=12, file=halogalfile)
-  read(12) dmhalo_id, lvl, dmhalo_mass, gal_id, gal_mass
-  associations(dmhalo_id, 1) = .true.
-  associations(dmhalo_id, 2) = lvl
-  associations(dmhalo_id, 3) = dmhalo_mass
-  associations(dmhalo_id, 4) = gal_id
-  associations(dmhalo_id, 5) = gal_mass
 
   !-------------------------------------
   ! Cleanup
   !-------------------------------------
   close(11)
   close(12)
-  deallocate(data)
+  close(13)
+  deallocate(data_gal)
+  deallocate(data_halo)
+  deallocate(associations)
 
 contains
   subroutine read_params ()
@@ -95,29 +100,36 @@ contains
 
     if (n < 4) then
        print*, 'Sort the galaxies between elliptics and spirals'
+       print*, ''
+       print*, 'Usage (all lists should come in binary format):'
+       print*, '\t -fga File to pick galaxies from'
+       print*, '\t -fdm File to pick dark matter halos from'
+       print*, '\t -fas File to get associations between dark matter halos and galaxies'
        !TODO: write usage
     end if
 
     do i = 1, n, 2
        call getarg(i, opt)
        if (i == n) then
-          print*,  '("option ",a2," has no argument")', opt
+          print*, '("option ",a2," has no argument")', opt
           stop
        end if
 
        call getarg(i+1, arg)
        select case(opt)
-       case ('-gal_list')
+       case ('-fga')
           gal_list_filename = trim(arg)
-       case ('-dm_halo_list')
+       case ('-fdm')
           dm_halo_list_filename = trim(arg)
+       case('-fas')
+          associations_filename = trim(arg)
        case ('-out')
           outfile = trim(arg)
        case ('-fin')
           fileinfo = trim(arg)
        case ('-halogal')
           halogalfile = trim(arg)
-       case ('-ver')
+       case ('-v')
           read (arg,*) verbose
        case default
           print '("unknown option ",a2," ignored")', opt
