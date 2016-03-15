72abe61a0572e62671100e68aadb2185fe8597e9
Corentin Cadiou <cadiou@horizon.iap.fr>
2016-03-15 18:29:55 +0100

    working hilbert curve

--- sort_galaxy.py
+++ sort_galaxy.py
@@ -7,6 +7,7 @@ import os.path
 from scipy.io import FortranFile
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import glob
 
 try:
     from tqdm import tqdm
@@ -20,8 +21,74 @@ parser.add_argument('--galaxy-list', type=str, required=True)
 parser.add_argument('--halo-list', type=str, required=True)
 parser.add_argument('--association-list', type=str, required=True)
 parser.add_argument('--ramses-output-start', type=str, required=True)
+parser.add_argument('--ramses-output-end', type=str, required=True)
 parser.add_argument('--DM-tree-bricks-start', '-dtbs', type=str, required=True)
 parser.add_argument('--DM-tree-bricks-end', '-dtbe', type=str, required=True)
+
+def hilbert3D(x, y, z, bit_length, npoint):
+    x_bit_mask = np.zeros(bit_length, dtype=bool)
+    y_bit_mask = np.zeros(bit_length, dtype=bool)
+    z_bit_mask = np.zeros(bit_length, dtype=bool)
+    i_bit_mask = np.zeros(3*bit_length, dtype=bool)
+    order = np.zeros(npoint)
+    state_diagram = np.array([ 1, 2, 3, 2, 4, 5, 3, 5,
+                               0, 1, 3, 2, 7, 6, 4, 5,
+                               2, 6, 0, 7, 8, 8, 0, 7,
+                               0, 7, 1, 6, 3, 4, 2, 5,
+                               0, 9,10, 9, 1, 1,11,11,
+                               0, 3, 7, 4, 1, 2, 6, 5,
+                               6, 0, 6,11, 9, 0, 9, 8,
+                               2, 3, 1, 0, 5, 4, 6, 7,
+                              11,11, 0, 7, 5, 9, 0, 7,
+                               4, 3, 5, 2, 7, 0, 6, 1,
+                               4, 4, 8, 8, 0, 6,10, 6,
+                               6, 5, 1, 2, 7, 4, 0, 3,
+                               5, 7, 5, 3, 1, 1,11,11,
+                               4, 7, 3, 0, 5, 6, 2, 1,
+                               6, 1, 6,10, 9, 4, 9,10,
+                               6, 7, 5, 4, 1, 0, 2, 3,
+                              10, 3, 1, 1,10, 3, 5, 9,
+                               2, 5, 3, 4, 1, 6, 0, 7,
+                               4, 4, 8, 8, 2, 7, 2, 3,
+                               2, 1, 5, 6, 3, 0, 4, 7,
+                               7, 2,11, 2, 7, 5, 8, 5,
+                               4, 5, 7, 6, 3, 2, 0, 1,
+                              10, 3, 2, 6,10, 3, 4, 4,
+                               6, 1, 7, 0, 5, 2, 4, 3]).reshape((12, 2, 8))
+    for ipt in range(npoint):
+        # convert to binary
+        for i in range(bit_length):
+            bit_mask = 2**i
+            x_bit_mask[i] = (x[ipt] & bit_mask) > 0
+            y_bit_mask[i] = (y[ipt] & bit_mask) > 0
+            z_bit_mask[i] = (z[ipt] & bit_mask) > 0
+
+        # Interleave bits
+        for i in range(bit_length):
+            i_bit_mask[3*i+2] = x_bit_mask[i]
+            i_bit_mask[3*i+1] = y_bit_mask[i]
+            i_bit_mask[3*i  ] = z_bit_mask[i]
+
+        cstate = 0
+        for i in range(bit_length-1, -1, -1):
+            b2 = 1 if i_bit_mask[3*i+2] else 0
+            b1 = 1 if i_bit_mask[3*i+1] else 0
+            b0 = 1 if i_bit_mask[3*i  ] else 0
+            sdigit = b2 * 4 + b1 * 2 + b0
+            nstate = state_diagram[cstate, 0, sdigit]
+            hdigit = state_diagram[cstate, 1, sdigit]
+            i_bit_mask[3*i+2] = hdigit & 0b100 > 0
+            i_bit_mask[3*i+1] = hdigit & 0b010 > 0
+            i_bit_mask[3*i  ] = hdigit & 0b001 > 0
+            print(nstate, sdigit, hdigit, cstate)
+            cstate = nstate
+
+        order[ipt] = 0
+        for i in range(0, 3*bit_length):
+            b0 = 1 if i_bit_mask[i] else 0
+            order[ipt] = order[ipt] + b0*2**i
+
+    return order
 
 # for cpu in range(1, ncpu+1):
 #     filename = 'part_{}.out{:0>5}'.format(nsim, cpu)
@@ -176,6 +243,18 @@ def read_association(listfile):
     assoc.gal_id.astype(int)
     return assoc
 
+def read_output(path):
+    f = FortranFile(path, 'r')
+    ncpu = f.read_ints()
+    dim = f.read_ints()
+    nparts = f.read_ints()
+    
+def find_particles(basepath, particles_to_read):
+    allfiles = glob.glob(os.path.join(basepath + 'part*.out*'))
+    allfiles.sort()
+
+    for f in tqdm(allfiles):
+        read_output(f)
 if __name__ == '__main__':
     args = parser.parse_args()
     print('Reading lists…')
@@ -190,7 +269,13 @@ if __name__ == '__main__':
     associations = read_association(args.association_list)
 
     print('Reading tree…')
-    halos_particles = particles_in_halo(args.DM_tree_bricks_start, start=181102)
+    # halos_particles = particles_in_halo(args.DM_tree_bricks_start, start=181102)
 
     print('Finding CPUs containing the particles…')
+    # read_output(args.ramses_output_end)
 
+    x = [ 7, 3, 9, 5, 5, 0, 8, 7, 5, 8 ]
+    y = [ 0, 1, 5, 6, 5, 7, 8, 3, 2, 9 ]
+    z = [ 2, 4, 2, 7, 4, 1, 0, 5, 7, 8 ]
+
+    print(hilbert3D(x, y, z, 1, 10))
