b6cf2f7e9d5d5a5b20869ac243cb634812818643
Corentin Cadiou <cadiou@horizon.iap.fr>
2016-03-17 18:24:26 +0100

    force the output format

--- create_listgal.f90
+++ create_listgal.f90
@@ -147,8 +147,8 @@ program create_listgal
   ! Output file
   nomfich=TRIM(outfich)
   write(*,*)'Writing file '//TRIM(nomfich)
-  open(unit=20,file=nomfich)
-  write(20,*) ngal, 7
+  open(unit=20, file=nomfich)
+  write(20,'(2i9)') ngal, 7
   do i=1,ngal
      write(20,'(2I9,1ES10.3,4f12.8)')idgal(i),hlevel(i),mgal(i),xgal(i),ygal(i),zgal(i),rgal(i)
   enddo
