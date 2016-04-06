9128784d8921017a2b63a83eb936f30b7e531e03
Corentin Cadiou <corentin.cadiou@cphyc.me>
2016-04-06 14:06:00 +0200

    use new parameters

--- get_halo_position.f90
+++ get_halo_position.f90
@@ -1,6 +1,8 @@
 program sort_galaxy
   use io
   use misc
+  use flap, only : command_line_interface
+
   implicit none
 
   !-------------------------------------
@@ -9,11 +11,15 @@ program sort_galaxy
   character(len=200) :: ramses_output_end, ramses_output_start, gal_list_filename, associations_filename, dm_halo_list_filename, brick_file, info_file_end, info_file_start, outfile, halo_to_cpu_file, mergertree_file
   integer :: param_from, param_to
   integer, parameter :: NPARTICLE_TO_PROBE_HALO = 50, NCPU_PER_HALO = 10
+
+  type(command_line_interface) :: cli
+
   !-------------------------------------
   ! List data
   !-------------------------------------
   real(kind=4), dimension(:,:), allocatable :: data_gal, data_associations, data_halo
   integer                                   :: ngal, nassoc, ndm_halo, gal_cols, assoc_cols, dm_halo_cols
+  real(kind=8) :: param_min_m, param_max_m
   !-------------------------------------
   ! Brick data
   !-------------------------------------
@@ -60,29 +66,36 @@ program sort_galaxy
   !-------------------------------------
   ! Read parameters
   !-------------------------------------
-  call read_params()
+  call parse_params(cli)
+  call cli%get(switch='--cpu-to', val=param_to)
+  call cli%get(switch='--cpu-from', val=param_from)
+  call cli%get(switch='--min-mass', val=param_min_m)
+  call cli%get(switch='--max-mass', val=param_max_m)
+  call cli%get(switch='--gal-list', val=gal_list_filename)
+  call cli%get(switch='--halo-list', val=dm_halo_list_filename)
+  call cli%get(switch='--association-list', val=associations_filename)
 
   !-------------------------------------
   ! Read lists
   !-------------------------------------
   print*, 'Reading file "' // trim(gal_list_filename) // '"'
-  call read_list_header(trim(gal_list_filename), ngal, gal_cols)
+  call read_list_header(trim(gal_list_filename), unit, ngal, gal_cols)
   allocate(data_gal(ngal, gal_cols))
-  call read_list_data(ngal, gal_cols, data_gal)
+  call read_list_data(unit, ngal, gal_cols, data_gal)
   print*, 'Got', ngal, 'galaxies in da pocket'
 
   print*, ''
   print*, 'Reading file "' // trim(associations_filename) // '"'
-  call read_list_header(trim(associations_filename), nassoc, assoc_cols)
+  call read_list_header(trim(associations_filename), unit, nassoc, assoc_cols)
   allocate(data_associations(nassoc, assoc_cols))
-  call read_list_data(nassoc, assoc_cols, data_associations)
+  call read_list_data(unit, nassoc, assoc_cols, data_associations)
   print*, 'Got', nassoc, 'associations in da pocket'
 
   print*, ''
   print*, 'Reading file "' // trim(dm_halo_list_filename) // '"'
-  call read_list_header(trim(dm_halo_list_filename), ndm_halo, dm_halo_cols)
+  call read_list_header(trim(dm_halo_list_filename), unit, ndm_halo, dm_halo_cols)
   allocate(data_halo(ndm_halo, dm_halo_cols))
-  call read_list_data(ndm_halo, dm_halo_cols, data_halo)
+  call read_list_data(unit, ndm_halo, dm_halo_cols, data_halo)
   print*, 'Got', ndm_halo, 'halos in da pocket'
 
   !-------------------------------------
@@ -135,6 +148,9 @@ program sort_galaxy
      halo_found = 0
 
      do i = 1, nDM
+        if (mDM(i) < param_min_m .or. mDM(i) > param_max_m) then
+           cycle
+        end if
         ! Pick 10 random particles and see if it's in the CPU
         do j = 1, NPARTICLE_TO_PROBE_HALO
            call random_number(tmp_real)
@@ -161,92 +177,109 @@ program sort_galaxy
   !$OMP END PARALLEL DO
   deallocate(tmp_arr)
 
+
+  !-------------------------------------
+  ! Simplify columns (find first empty column)
+  !-------------------------------------
+  tmp_int = NCPU_PER_HALO
   allocate(tmp_arr(NCPU_PER_HALO))
+  tmp_arr = maxval(halo_to_cpu, 1)
+  do i = 1, NCPU_PER_HALO
+     if (tmp_arr(i) == 0) then
+        tmp_int = i
+        exit
+     end if
+  end do
+
+  print*, 'Found ', tmp_int, 'empty columns'
+  !-------------------------------------
+  ! Only save non empty columns
+  !-------------------------------------
   write(tmp_char, '(a, i0.5, a, i0.5)') "out", param_from, '-', param_to
   open(10, file=tmp_char)
-  write(10, *) nDM, NCPU_PER_HALO
+  write(10, *) nDM, tmp_int
   do i = 1, nDM
      tmp_arr = halo_to_cpu(i, :)
-     write(10, *) idDM(i), tmp_arr
+     write(10, *) idDM(i), tmp_arr(:tmp_int)
   end do
   close(10)
   deallocate(tmp_arr)
 
-  call write_list(trim(tmp_char) // '.bin', nDM, NCPU_PER_HALO, halo_to_cpu)
+  call write_list(trim(tmp_char) // '.bin', nDM, tmp_int, halo_to_cpu)
 
 contains
-  subroutine read_params ()
-    integer            :: i,n
-    integer            :: iargc
-    character(len=4)   :: opt
-    character(len=128) :: arg
-    n = iargc()
+  ! subroutine read_params ()
+  !   integer            :: i,n
+  !   integer            :: iargc
+  !   character(len=4)   :: opt
+  !   character(len=128) :: arg
+  !   n = iargc()
 
-    ! if (n < 4) then
-    !    print*, 'Sort the galaxies between elliptics and spirals'
-    !    print*, ''
-    !    print*, 'Usage (all lists should come in binary format):'
-    !    print*, '\t -fga File to pick galaxies from'
-    !    print*, '\t -fdm File to pick dark matter halos from'
-    !    print*, '\t -fas File to get associations between dark matter halos and galaxies'
-    !    !TODO: write usage
-    ! end if
+  !   ! if (n < 4) then
+  !   !    print*, 'Sort the galaxies between elliptics and spirals'
+  !   !    print*, ''
+  !   !    print*, 'Usage (all lists should come in binary format):'
+  !   !    print*, '\t -fga File to pick galaxies from'
+  !   !    print*, '\t -fdm File to pick dark matter halos from'
+  !   !    print*, '\t -fas File to get associations between dark matter halos and galaxies'
+  !   !    !TODO: write usage
+  !   ! end if
 
-    !-------------------------------------
-    ! default values
-    !-------------------------------------
-    gal_list_filename = "lists/list_kingal_00782.dat"
-    dm_halo_list_filename = "lists/list_halo.dat.bin"
-    associations_filename = "lists/associated_halogal_782.dat.bin"
-    ramses_output_start = "/data52/Horizon-AGN/OUTPUT_DIR/output_00002"
-    ramses_output_end = "/data52/Horizon-AGN/OUTPUT_DIR/output_00782"
-    brick_file = "/data52/Horizon-AGN/TREE_DM_celldx2kpc_SC0.9r/tree_bricks782"
-    info_file_end = '/data52/Horizon-AGN/OUTPUT_DIR/output_00782/info_00782.txt'
-    info_file_start = '/data52/Horizon-AGN/OUTPUT_DIR/output_00002/info_00002.txt'
-    halo_to_cpu_file = 'lists/halo_to_cpu.00002.raw.dat.bin'
-    mergertree_file = '/data33/dubois/H-AGN/MergerTree/TreeMaker_HAGN/tree.dat'
+  !   !-------------------------------------
+  !   ! default values
+  !   !-------------------------------------
+  !   gal_list_filename = "lists/list_kingal_00782.dat"
+  !   dm_halo_list_filename = "lists/list_halo.dat.bin"
+  !   associations_filename = "lists/associated_halogal_782.dat.bin"
+  !   ramses_output_start = "/data52/Horizon-AGN/OUTPUT_DIR/output_00002"
+  !   ramses_output_end = "/data52/Horizon-AGN/OUTPUT_DIR/output_00782"
+  !   brick_file = "/data52/Horizon-AGN/TREE_DM_celldx2kpc_SC0.9r/tree_bricks782"
+  !   info_file_end = '/data52/Horizon-AGN/OUTPUT_DIR/output_00782/info_00782.txt'
+  !   info_file_start = '/data52/Horizon-AGN/OUTPUT_DIR/output_00002/info_00002.txt'
+  !   halo_to_cpu_file = 'lists/halo_to_cpu.00002.raw.dat.bin'
+  !   mergertree_file = '/data33/dubois/H-AGN/MergerTree/TreeMaker_HAGN/tree.dat'
 
-    param_from = 1
-    param_to = -1
+  !   param_from = 1
+  !   param_to = -1
 
-    do i = 1, n, 2
-       call getarg(i, opt)
-       if (i == n) then
-          print*, '("option ",a2," has no argument")', opt
-          stop
-       end if
+  !   do i = 1, n, 2
+  !      call getarg(i, opt)
+  !      if (i == n) then
+  !         print*, '("option ",a2," has no argument")', opt
+  !         stop
+  !      end if
 
-       call getarg(i+1, arg)
-       select case(opt)
-       case ('-fga')
-          gal_list_filename = trim(arg)
-       case ('-fdm')
-          dm_halo_list_filename = trim(arg)
-       case('-fas')
-          associations_filename = trim(arg)
-       case('-ods') ! Ramses output start
-          ramses_output_start = trim(arg)
-       case('-ode') ! Ramses output start
-          ramses_output_end = trim(arg)
-       case('-bri')
-          brick_file = trim(arg)
-       case('-ifs')
-          info_file_start = trim(arg)
-       case('-ife')
-          info_file_end = trim(arg)
-       case ('-out')
-          outfile = trim(arg)
-       case ('-fro')
-          tmp_char = trim(arg)
-          read(tmp_char, '(i10)') param_from
-       case ('-to')
-          tmp_char = trim(arg)
-          read(tmp_char, '(i10)') param_to
-       case default
-          print '("unknown option ",a2," ignored")', opt
-       end select
-    end do
+  !      call getarg(i+1, arg)
+  !      select case(opt)
+  !      case ('-fga')
+  !         gal_list_filename = trim(arg)
+  !      case ('-fdm')
+  !         dm_halo_list_filename = trim(arg)
+  !      case('-fas')
+  !         associations_filename = trim(arg)
+  !      case('-ods') ! Ramses output start
+  !         ramses_output_start = trim(arg)
+  !      case('-ode') ! Ramses output start
+  !         ramses_output_end = trim(arg)
+  !      case('-bri')
+  !         brick_file = trim(arg)
+  !      case('-ifs')
+  !         info_file_start = trim(arg)
+  !      case('-ife')
+  !         info_file_end = trim(arg)
+  !      case ('-out')
+  !         outfile = trim(arg)
+  !      case ('-fro')
+  !         tmp_char = trim(arg)
+  !         read(tmp_char, '(i10)') param_from
+  !      case ('-to')
+  !         tmp_char = trim(arg)
+  !         read(tmp_char, '(i10)') param_to
+  !      case default
+  !         print '("unknown option ",a2," ignored")', opt
+  !      end select
+  !   end do
 
-  end subroutine read_params
+  ! end subroutine read_params
 
 end program sort_galaxy
