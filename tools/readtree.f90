8b7c8dc00a87bd3a290bb8103fabc56d3fdc9742
Corentin Cadiou <corentin.cadiou@cphyc.me>
2016-05-02 11:11:45 +0200

    Comment and tidy up readtree module

--- tools/readtree.f90
+++ tools/readtree.f90
@@ -3,25 +3,10 @@ module readTree
   public
   integer :: nsteps
 
-  integer, dimension(:), allocatable       :: nb_of_halos, nb_of_subhalos
-  real (kind=4), dimension(:), allocatable :: aexp, age_univ, omega_t
+  integer, dimension(:), allocatable       :: nb_of_halos, nb_of_subhalos !> Number of halos and subhalos in the tree
+  real (kind=4), dimension(:), allocatable :: aexp, age_univ, omega_t     !> Parameters of each outputs
 
-  ! type HALO_LIST_T
-  !    type(HALO_T), pointer :: halo
-  ! end type HALO_LIST_T
-
-  ! type HALO_T
-  !    type(HALO_T), pointer          :: parent
-  !    type(HALO_LIST_T), allocatable :: childs(:)
-  !    integer                        :: mynumber, BushID, mystep, level, hosthalo, hostsub, &
-  !         nbsub, nextsub, nb_of_fathers, nb_of_son
-  !    integer, allocatable           :: idfather(:), idson(:)
-  !    real(kind=4)                   :: m, macc, px, py, pz, vx, vy, vz, Lx, Ly, Lz, &
-  !         r, ra, rb, rc, ek, ep, et, spin
-  !    real(kind=4), allocatable      :: m_father(:)
-  !    integer, allocatable           :: id_father(:), idson(:)
-  ! end type HALO_T
-
+  ! Internal variables
   integer :: istep = 1
   integer :: j = 1
   integer :: unit
@@ -31,9 +16,10 @@ module readTree
   real(kind=4), allocatable  :: tmp_m_father(:)
 
 contains
+  !> Read a merger tree, storing the data about it in the module.
   subroutine read_tree(filename)
 
-    character(len=*), intent(in) :: filename
+    character(len=*), intent(in) :: filename !> Tree filename
 
     open(newunit=unit, file=trim(filename), status='old', form='unformatted')
     read(unit) nsteps
@@ -50,15 +36,32 @@ contains
 
   end subroutine read_tree
 
-  subroutine iter_tree(stop_now, mynumber, BushID, mystep, level, hosthalo, hostsub, &
+  !> Read the next part of the tree. If the end is reached, set stop_now to True.
+  !! To get the father / son of the halo, call directly after 'get_father' or 'get_son'
+  subroutine iter_tree(stop_now, halo_id, BushID, step, level, hosthalo, hostsub, &
        nbsub, nextsub, nb_of_fathers, nb_of_son, &
        m, macc, px, py, pz, vx, vy, vz, Lx, Ly, Lz, &
        r, ra, rb, rc, ek, ep, et, spin)
-    logical, intent(out)         :: stop_now
-    integer(kind=4), intent(out) :: mynumber, BushID, mystep, level, hosthalo, hostsub
-    integer(kind=4), intent(out) :: nbsub, nextsub, nb_of_fathers, nb_of_son
-    real(kind=4), intent(out)    :: m, macc, px, py, pz, vx, vy, vz, Lx, Ly, Lz
-    real(kind=4), intent(out)    :: r, ra, rb, rc, ek, ep, et, spin
+
+    logical, intent(out), optional         :: stop_now      !> true if end is reached
+    integer(kind=4), intent(out), optional :: halo_id       !> id if the halo
+    integer(kind=4), intent(out), optional :: BushID        !> ???
+    integer(kind=4), intent(out), optional :: step          !> current step
+    integer(kind=4), intent(out), optional :: level         !> level of the halo (1 is halo, 2+ are subhalos)
+    integer(kind=4), intent(out), optional :: hosthalo      !> id of hosting halo
+    integer(kind=4), intent(out), optional :: hostsub       !> ???
+    integer(kind=4), intent(out), optional :: nbsub         !> number of subhalos
+    integer(kind=4), intent(out), optional :: nextsub       !> ???
+    integer(kind=4), intent(out), optional :: nb_of_fathers
+    integer(kind=4), intent(out), optional :: nb_of_son
+    real(kind=4), intent(out), optional    :: m             !> Mass of the halo
+    real(kind=4), intent(out), optional    :: macc          !> Accreted mass
+    real(kind=4), intent(out), optional    :: px, py, pz    !> position of the halo
+    real(kind=4), intent(out), optional    :: vx, vy, vz    !> velocitiy of the halo
+    real(kind=4), intent(out), optional    :: Lx, Ly, Lz    !> angular momentum of the halo
+    real(kind=4), intent(out), optional    :: r, ra, rb, rc !> ellispoid description
+    real(kind=4), intent(out), optional    :: ek, ep, et    !> Kinetic, potential and thermic energy
+    real(kind=4), intent(out), optional    :: spin          !> spin of the halo
 
     integer :: ntot
 
@@ -77,9 +80,9 @@ contains
        end if
     end if
 
-    read(unit) mynumber
+    read(unit) halo_id
     read(unit) BushID
-    read(unit) mystep
+    read(unit) step
     read(unit) level, hosthalo, hostsub, nbsub, nextsub
     read(unit) m
     read(unit) macc
@@ -117,29 +120,31 @@ contains
     ! if(star) read(unit)
   end subroutine iter_tree
 
-  subroutine get_fathers(nb_of_fathers, id_father, m_father)
-    integer, intent(in) :: nb_of_fathers
+  !> Get the father of the last red halo
+  subroutine get_father(nb_of_fathers, id_father, m_father)
+    integer, intent(in)                                 :: nb_of_fathers !> number of son of the halo
+    integer, dimension(nb_of_fathers), intent(out)      :: id_father     !> id of the father
+    real(kind=4), dimension(nb_of_fathers), intent(out) :: m_father      !> mass of the father
 
-    integer, dimension(nb_of_fathers), intent(out)      :: id_father
-    real(kind=4), dimension(nb_of_fathers), intent(out) :: m_father
-
-    !f2py depend(nb_of_fathers) :: id_father, m_father
     id_father = tmp_id_father
     m_father = tmp_m_father
-  end subroutine get_fathers
+  end subroutine get_father
 
+  !> Get the son of the last red halo
   subroutine get_son(nb_of_son, id_son)
-    integer, intent(in) :: nb_of_son
+    integer, intent(in)                        :: nb_of_son !> number of son of the halo
+    integer, dimension(nb_of_son), intent(out) :: id_son    !> ids of the son of the halo
 
-    integer, dimension(nb_of_son), intent(out) :: id_son
     id_son = tmp_id_son
   end subroutine get_son
 
+  !> Close the tree file
   subroutine close_file()
     ! TODO: inquire it is opened
     close(unit)
   end subroutine close_file
 
+  !> Free allocated variables
   subroutine free()
     if (allocated(nb_of_halos    )) deallocate(nb_of_halos)
     if (allocated(nb_of_subhalos )) deallocate(nb_of_subhalos)
