0a031c77f74efe4ae57040af2a28bed543d31a4e
Corentin Cadiou <corentin.cadiou@cphyc.me>
2016-05-02 18:18:34 +0200

    Add reverse tree subroutine

--- tools/readtree.f90
+++ tools/readtree.f90
@@ -1,10 +1,13 @@
 module readTree
   implicit none
   public
-  integer :: nsteps
+  integer :: nsteps !> Number of steps in the simulation
 
-  integer, dimension(:), allocatable       :: nb_of_halos, nb_of_subhalos !> Number of halos and subhalos in the tree
-  real (kind=4), dimension(:), allocatable :: aexp, age_univ, omega_t     !> Parameters of each outputs
+  integer, dimension(:), allocatable       :: nb_of_halos    !> halos in the tree
+  integer, dimension(:), allocatable       :: nb_of_subhalos !> subhalos in the tree
+  real (kind=4), dimension(:), allocatable :: aexp     !> expansion factor at each step
+  real (kind=4), dimension(:), allocatable :: age_univ !> age of the universe at each step
+  real (kind=4), dimension(:), allocatable :: omega_t  !> omega_t at at each step
 
   ! Internal variables
   integer :: istep = 1
@@ -15,6 +18,7 @@ module readTree
   integer, allocatable       :: tmp_id_father(:), tmp_id_son(:)
   real(kind=4), allocatable  :: tmp_m_father(:)
 
+  private :: istep, j, unit, tmp_id_father, tmp_id_son, tmp_m_father
 contains
   !> Read a merger tree, storing the data about it in the module.
   subroutine read_tree(filename)
@@ -74,7 +78,7 @@ contains
        j = 1
        istep = istep + 1
        if (istep > nsteps) then
-          close(unit)
+          call close_file()
           stop_now = .true.
           return
        end if
@@ -140,8 +144,10 @@ contains
 
   !> Close the tree file
   subroutine close_file()
+    logical :: op
     ! TODO: inquire it is opened
-    close(unit)
+    inquire(unit, opened=op)
+    if (op) close(unit)
   end subroutine close_file
 
   !> Free allocated variables
@@ -153,4 +159,183 @@ contains
     if (allocated(age_univ       )) deallocate(age_univ)
   end subroutine free
 
+  !> Revert the tree (from end to begin)
+  subroutine revert_steps(filetree, outfich, star)
+    character(LEN=128), intent(in) :: outfich, filetree
+    logical, intent(in)            :: star
+
+    integer                                    :: unit_in, unit_out
+    integer                                    :: ntot, i, j, istep, idone
+    integer(kind=4)                            :: nsteps, nbodies, nb_of_fathers, nb_of_sons, ncont
+    integer(kind=4)                            :: mynumber, BushID, mystep, level, hosthalo, hostsub, nbsub, nextsub
+    real(kind=4)                               ::m, macc, px, py, pz, vx, vy, vz, Lx, Ly, Lz, r, ra, rb, rc, ek, ep, et, spin
+    real(kind=4)                               ::rvir, mvir, tvir, cvel, rho0, rc2
+    integer(kind=4), allocatable, dimension(:) :: nb_of_halos, nb_of_subhalos
+    integer(kind=4), allocatable, dimension(:) :: idfather, idson
+    real   (kind=4), allocatable, dimension(:) :: aexp, age_univ, omega_t, mfather
+    integer(kind=4), allocatable, dimension(:) :: nb_of_halos_rev, nb_of_subhalos_rev
+    real   (kind=4), allocatable, dimension(:) :: aexp_rev, age_univ_rev, omega_t_rev
+
+    !-----------------------------------------------
+    ! Lecture du fichier contenant les galaxies
+    !-----------------------------------------------
+    write(*,*)filetree
+
+    open(newunit=unit_in,file=filetree,status='old',form='unformatted')
+    read(unit_in) nsteps
+    allocate(nb_of_halos(1:nsteps),nb_of_subhalos(1:nsteps),aexp(1:nsteps),omega_t(1:nsteps),age_univ(1:nsteps))
+
+    read(unit_in) nb_of_halos(1:nsteps), nb_of_subhalos(1:nsteps)
+    read(unit_in) aexp(1:nsteps)
+    read(unit_in) omega_t(1:nsteps)
+    read(unit_in) age_univ(1:nsteps)
+    write(*,*)nsteps,' steps'
+    write(*,*)'list of timesteps:'
+    do i=1,nsteps
+       write(*,*)1d0/aexp(i)-1d0,age_univ(i)
+    enddo
+    write(*,*)MINVAL(nb_of_halos),MAXVAL(nb_of_halos),' galaxies'
+    write(*,*)MINVAL(nb_of_subhalos),MAXVAL(nb_of_subhalos),' sub-galaxies'
+    close(unit_in)
+
+    allocate(nb_of_halos_rev(1:nsteps),nb_of_subhalos_rev(1:nsteps))
+    allocate(aexp_rev(1:nsteps),omega_t_rev(1:nsteps),age_univ_rev(1:nsteps))
+    do i=1,nsteps
+       nb_of_halos_rev   (nsteps-i+1)=nb_of_halos   (i)
+       nb_of_subhalos_rev(nsteps-i+1)=nb_of_subhalos(i)
+       aexp_rev    (nsteps-i+1)=aexp    (i)
+       omega_t_rev (nsteps-i+1)=omega_t (i)
+       age_univ_rev(nsteps-i+1)=age_univ(i)
+    enddo
+
+    open(newunit=unit_out,file=outfich,form='unformatted')
+    write(unit_out) nsteps
+    write(unit_out) nb_of_halos_rev(1:nsteps), nb_of_subhalos_rev(1:nsteps)
+    write(unit_out) aexp_rev(1:nsteps)
+    write(unit_out) omega_t_rev(1:nsteps)
+    write(unit_out) age_univ_rev(1:nsteps)
+    deallocate(nb_of_halos_rev,nb_of_subhalos_rev,aexp_rev,omega_t_rev,age_univ_rev)
+
+    idone=nsteps
+
+    do while(idone>0)
+
+       open(unit=unit_in,file=filetree,status='old',form='unformatted')
+       read(unit_in) nsteps
+       read(unit_in) nb_of_halos(1:nsteps), nb_of_subhalos(1:nsteps)
+       read(unit_in) aexp(1:nsteps)
+       read(unit_in) omega_t(1:nsteps)
+       read(unit_in) age_univ(1:nsteps)
+
+       do istep=1,idone
+
+          ntot=nb_of_halos(istep)+nb_of_subhalos(istep)
+
+          if(istep==idone)then
+
+             do j=1,ntot
+                read(unit_in) mynumber
+                read(unit_in) BushID
+                read(unit_in) mystep
+                read(unit_in) level,hosthalo,hostsub,nbsub,nextsub
+                read(unit_in) m
+                read(unit_in) macc
+                read(unit_in) px,py,pz
+                read(unit_in) vx,vy,vz
+                read(unit_in) Lx,Ly,Lz
+                read(unit_in) r,ra,rb,rc
+                read(unit_in) ek,ep,et
+                read(unit_in) spin
+
+                write(unit_out) mynumber
+                write(unit_out) BushID
+                write(unit_out) mystep
+                write(unit_out) level,hosthalo,hostsub,nbsub,nextsub
+                write(unit_out) m
+                write(unit_out) macc
+                write(unit_out) px,py,pz
+                write(unit_out) vx,vy,vz
+                write(unit_out) Lx,Ly,Lz
+                write(unit_out) r,ra,rb,rc
+                write(unit_out) ek,ep,et
+                write(unit_out) spin
+
+                read(unit_in) nb_of_fathers
+                write(unit_out) nb_of_fathers
+                if(nb_of_fathers>0)then
+                   allocate(idfather(1:nb_of_fathers),mfather(1:nb_of_fathers))
+                   read(unit_in) idfather(1:nb_of_fathers)
+                   read(unit_in) mfather (1:nb_of_fathers)
+                   write(unit_out) idfather(1:nb_of_fathers)
+                   write(unit_out) mfather (1:nb_of_fathers)
+                   deallocate(idfather,mfather)
+                endif
+
+                read(unit_in) nb_of_sons
+                write(unit_out) nb_of_sons
+                if(nb_of_sons>0)then
+                   allocate(idson(1:nb_of_sons))
+                   read(unit_in) idson(1:nb_of_sons)
+                   write(unit_out) idson(1:nb_of_sons)
+                   deallocate(idson)
+                endif
+
+                read(unit_in)rvir,mvir,tvir,cvel
+                read(unit_in)rho0,rc2
+                if(star)read(unit_in)ncont
+
+                write(unit_out)rvir,mvir,tvir,cvel
+                write(unit_out)rho0,rc2
+                if(star)write(unit_out)ncont
+             enddo
+
+             write(*,*)'wrote ',idone,1d0/aexp(idone)-1d0
+             idone=idone-1
+
+          else
+
+             do j=1,ntot
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+                read(unit_in)
+
+                read(unit_in) nb_of_fathers
+                if(nb_of_fathers>0)then
+                   allocate(idfather(1:nb_of_fathers),mfather(1:nb_of_fathers))
+                   read(unit_in) idfather(1:nb_of_fathers)
+                   read(unit_in) mfather (1:nb_of_fathers)
+                   deallocate(idfather,mfather)
+                endif
+
+                read(unit_in) nb_of_sons
+                if(nb_of_sons>0)then
+                   allocate(idson(1:nb_of_sons))
+                   read(unit_in) idson(1:nb_of_sons)
+                   deallocate(idson)
+                endif
+
+                read(unit_in)
+                read(unit_in)
+                if(star)read(unit_in)
+             enddo
+
+          endif
+
+       enddo
+       close(unit_in)
+
+    enddo
+    close(unit_out)
+
+
+  end subroutine revert_steps
 end module readTree
